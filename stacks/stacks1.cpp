f2db649d458c23c950baa29984361d5988e2def8
nagadeepak511 <nagadeepak511@gmail.com>
2021-08-12 22:12:23 +0530

    stacks using arrays

---
+++ stacks/stacks1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+struct Stack{
+    int size, top, elements[50];
+
+    void push(int element){
+        if(top == size-1) cout << "Size exceeded\n";
+        else{
+            elements[top+1] = element;
+            top++;
+        }
+    }
+
+    int pop(){
+        if(top < 0){
+            cout << "Empty stack\n";
+            return 420;
+        }
+        else{
+            top--;
+            return elements[top+1];
+        }
+    }
+
+    int peek(){
+        if(top < 0){
+            cout << "Empty stack";
+            return 420;
+        }
+        else return elements[top];
+    }
+};
+
+int main(){
+    struct Stack s;
+}
